7800eaab8e01bf587c6d54c1b3ba1aa7c40c00ed
Marcos Mundina Fabregat <discor2008125@gmail.com>
2026-05-24 00:23:16 +0200

    Finally got camera movement to work

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -3,8 +3,43 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
-	private const float Speed = 10.0f;
-	private const float JumpVelocity = 4.5f;
+	private const float Speed = 7.0f;
+	private const float JumpVelocity = 5f;
+	private const float CameraSensitivity = 0.005f;
+
+	private Node3D head;
+	private Camera3D camera;
+
+	public override void _Ready()
+	{
+		head = GetNode<Node3D>("Head");
+		camera = GetNode<Camera3D>("Head/Camera3D");
+	}
+
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		//Manage control of the mouse cursor
+		if (@event is InputEventMouseButton)
+		{
+			Input.MouseMode = Input.MouseModeEnum.Captured;
+		}
+		else if (@event.IsActionPressed("ui_cancel"))
+		{
+			Input.MouseMode = Input.MouseModeEnum.Visible;
+		}
+		
+		//Actual camera movement
+		if (@event is InputEventMouseMotion && Input.MouseMode == Input.MouseModeEnum.Captured)
+		{
+			InputEventMouseMotion motion = (InputEventMouseMotion)@event;
+			
+			head.RotateY(-motion.Relative.X * CameraSensitivity);
+			camera.RotateX(-motion.Relative.Y * CameraSensitivity);
+
+			Vector3 camRot = new Vector3(Mathf.Clamp(camera.Rotation.X, Mathf.DegToRad(-80), Mathf.DegToRad(80)), 0, 0);
+			camera.Rotation = camRot;
+		}
+	}
 	
 	public override void _PhysicsProcess(double delta)
 	{
@@ -19,9 +54,9 @@ public partial class Player : CharacterBody3D
 		{
 			velocity.Y = JumpVelocity;
 		}
-
+		
 		Vector2 inputDir = Input.GetVector("Left", "Right", "Forward", "Backward");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		Vector3 direction = (head.GlobalTransform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
 		if (direction != Vector3.Zero)
 		{
 			velocity.X = direction.X * Speed;
